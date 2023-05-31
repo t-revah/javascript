ffd3f1317451291d4421af59694a2bb8ba9ed48d
t-revah <tomyrevah@gmail.com>
2023-05-30 21:04:51 -0300

    Calculadora

---
+++ Calculadora/main.js
@@ -0,0 +1,17 @@
+console.log("Hola");
+
+let numero1 = prompt ("NRO: ");
+let numero2 = prompt ("NRO: ");
+numero1 = parseFloat (numero1); // es una funcion que convierte una cadena de texto en un numero
+numero2 = parseFloat (numero2);
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Suma:" + suma);
+console.log("Resta:" + resta);
+console.log("Multiplicacion:" + multiplicacion);
+console.log("Division:" + division);
+
